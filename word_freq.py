8ba03a9c3aa81354a09faae5feea541729fa9809
ksg2388 <ksg2388@naver.com>
2022-04-13 21:29:03 +0900

    한 줄씩 읽어서 단어별로 나누기 기능 추가

--- word_freq.py
+++ word_freq.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
 import sys
+import re
+
 if __name__=='__main__':
     argument = sys.argv
 
-    if len(argument) == 3:
-        print(argument[1], argument[2])
-    else:
+    if len(argument) != 3:
         sys.exit("인자 개수가 틀립니다.")
 
 wordlist = []
 f = open(argument[1], 'r')
 
 x = f.readlines()
+
+for i in x:
+    testlist = (re.split(r'[\W]', i))
+    for j in testlist:
+        wordlist.append(j)
+
+print(wordlist)
 
 f.close()
