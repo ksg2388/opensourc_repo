d1c29b16814c7533ed792d3e95abd3d3b16e6467
ksg2388 <ksg2388@naver.com>
2022-04-13 19:58:57 +0900

    파라미터 입력받기 추가

---
+++ word_freq.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+if __name__=='__main__':
+    argument = sys.argv
+
+    if len(argument) == 3:
+        print(argument[1], argument[2])
+    else:
+        sys.exit("인자 개수가 틀립니다.")
